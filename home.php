ff02a2423572c2ec8af384362a7b5180f6f7b4c7
Soumaya Boukadida <soumaya.boukadida@insat.ucar.tn>
2023-04-10 18:50:50 +0100

    2nd commit

--- home.php
+++ home.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1" />
     <title>Travel Website</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,500;0,600;1,300&display=swap" rel="stylesheet">
@@ -37,7 +39,7 @@
             </div>
             <div class="video">
                 <video id="video" autoplay loop muted>
-                    <source src="photos/video1.mp4" type="video/mp4" />
+                    <source src="photos/video2.mp4" type="video/mp4" />
                 </video>
             </div>
         </div>
