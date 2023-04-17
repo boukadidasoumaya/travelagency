32e701b30a1f27e46fdc4ad19bd02a367e559652
boukadida soumaya <soumaya.boukadida@insat.ucar.tn>
2023-04-17 13:30:25 +0100

    .

--- fragments/footer.php
+++ fragments/footer.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <head>
-<title>footer-home</title>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+<head>
+    <title>footer-home</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="footer css.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="footer.css">
 
 
-    </head>
-    <body>
-      <div class="container">
+</head>
+
+<body>
+    <div class="container">
         <div class="links">
             <p class="text">Useful links:</p>
             <br>
@@ -30,17 +32,17 @@
 
             <input class="email" type="email" placeholder="Email" name="email"><br>
             <button>submit</button><br>
-            <input class="msg"  type="text" placeholder="write a message" name="msg"><br>
+            <input class="msg" type="text" placeholder="write a message" name="msg"><br>
             <button>Send Message</button>
         </div>
         <div class="follow">
-           <p class="text"> Follow Us:</p>
+            <p class="text"> Follow Us:</p>
             <div class="sc">
-            <a class="fab fa-instagram" href="https://www.instagram.com/accounts/login/"></a><br>
-            <!--la classe "fab fa-facebook" est une classe css fournie par la bibliotheque de polices d'icones Font Awesome pour l'icone du facebook-->
-            <a href="https://fr-fr.facebook.com/login/" class="fab fa-facebook"></a><br>
-            <!--la classe "fab fa-twitter" est une classe css fournie par la bibliotheque de polices d'icones Font Awesome pour l'icone du twitter-->
-            <a href="https://twitter.com/home" class="fab fa-twitter"></a>
+                <a class="fab fa-instagram" href="https://www.instagram.com/accounts/login/"></a><br>
+                <!--la classe "fab fa-facebook" est une classe css fournie par la bibliotheque de polices d'icones Font Awesome pour l'icone du facebook-->
+                <a href="https://fr-fr.facebook.com/login/" class="fab fa-facebook"></a><br>
+                <!--la classe "fab fa-twitter" est une classe css fournie par la bibliotheque de polices d'icones Font Awesome pour l'icone du twitter-->
+                <a href="https://twitter.com/home" class="fab fa-twitter"></a>
             </div>
         </div>
         <div class="video">
@@ -49,12 +51,12 @@
             </video>
         </div>
     </div>
-        
 
 
 
 
-    </body>
+
+</body>
 
 
 </html>
