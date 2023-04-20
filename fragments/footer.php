17abd7eafd3094b11bb2f677cc0253ec2129298a
arijzaidi <arij.zaidi@insat.ucar.tn>
2023-04-20 10:43:08 +0100

    .

--- fragments/footer.php
+++ fragments/footer.php
@@ -1,56 +1,69 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-    <title>footer-home</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
-
-    <link rel="stylesheet" href="footer.css">
-
-
-</head>
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="stylesheet" href="newfooter.css">
+        <link rel="stylesheet" href="chemin/vers/font-awesome/css/all.min.css">
+        <script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
+    </head>
 
 <body>
-    <div class="container2">
-        <div class="links">
-            <p class="text">Useful links:</p>
-            <br>
-            <div class="home">
-                <a href="C:\Users\DELL\Documents\travelagency\home\home2.php">Home</a><br>
-                <a href="C:\Users\DELL\Documents\travelagency\home\home2.php#about">About us</a><br>
-                <a href="C:\Users\DELL\Documents\travelagency\home\home2.php#book now">Book now</a><br>
-                <a href="C:\Users\DELL\Documents\travelagency\home\home2.php#slides-par"> Our trips</a><br>
-                <a href="C:\Users\DELL\Documents\travelagency\login.html">Login</a><br>
+    <footer>
+        <div class="row" class="logo">
+            
+            <div class="col">
+                <img src="think travel1.png" width="200px">
+                <ul class="txt">
+                  <li>"Embark on an unforgettable adventure with us</li>
+                  <li> and create memories that will last a lifetime.</li>
+                  <li>We strive to provide exceptional</li>
+                  <li>service and an unparalleled travel experience."</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Office</h3>
+                <ul>
+                    <li>think travel</li>
+                    <li>tunis,tunisie</li>
+                    <li>Centre Urbain Nord</li>
+                    <li class="email">thinktravel@gmail.com</li>
+                    <li><h4>- 01234567</h4></li>
+            </div>
+            <div class="col">
+                <h3>useful links</h3>
+                <ul class="link">
+                    <li><a href="C:\Users\DELL\Documents\travelagency\home\home2.php">Home</a></li>
+                    <li><a href="C:\Users\DELL\Documents\travelagency\home\home2.php#about">About us</a>
+                    </li>
+                    <li><a href="C:\Users\DELL\Documents\travelagency\home\home2.php#book now">Book now</a>
+                    </li>
+                    <li><a href="C:\Users\DELL\Documents\travelagency\home\home2.php#slides-par"> Our trips</a>
+                    </li>
+                    <li><a href="C:\Users\DELL\Documents\travelagency\login.html">Login</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Follow us</h3>
+                <form>
+                    <input type="email" placeholder="connect with your email" required>
+                    <button type="submit">send</button> 
+                </form>
+                <div class="contact">
+                    <a class="fab fa-facebook" href="https://fr-fr.facebook.com/login/"></a>
+                    <a class="fab fa-instagram" href="https://www.instagram.com/accounts/login/"></a>
+                    <a class="fab fa-twitter" href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZnIifQ%3D%3D%22%7D"></a>
+                    <a class="fab fa-whatsapp" href="whatsapp.com"></a>
+                    
+
+
+                </div>
             </div>
         </div>
-        <div class="mail">
-            <p class="text">Contact Us</p><br>
-
-            <input class="email" type="email" placeholder="Email" name="email"><br>
-            <button>submit</button><br>
-            <input class="msg" type="text" placeholder="write a message" name="msg"><br>
-            <button>Send Message</button>
-        </div>
-        <div class="follow">
-            <p class="text"> Follow Us:</p>
-            <div class="sc">
-                <a class="fab fa-instagram" href="https://www.instagram.com/accounts/login/"></a><br>
-                <!--la classe "fab fa-facebook" est une classe css fournie par la bibliotheque de polices d'icones Font Awesome pour l'icone du facebook-->
-                <a href="https://fr-fr.facebook.com/login/" class="fab fa-facebook"></a><br>
-                <!--la classe "fab fa-twitter" est une classe css fournie par la bibliotheque de polices d'icones Font Awesome pour l'icone du twitter-->
-                <a href="https://twitter.com/home" class="fab fa-twitter"></a>
-            </div>
-        </div>
-    </div>
-
-
-
-
-
+        <!--<hr> is used to add a line at the bottom of the page-->
+        <hr>
+        <p class="copyright">think travel © copyright 2023 - All Rights Reserved</p>
+    </footer>
 </body>
 
 
