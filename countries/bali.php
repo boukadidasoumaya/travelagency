189082e5e6780c496d721a9bdab5df9aee9f9242
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2023-04-21 05:15:47 +0100

    bali

--- countries/bali.php
+++ countries/bali.php
@@ -1,17 +1,18 @@
 <?php
-$country_name = "Indonesia-Bali";
+$city_name = "Bali";
+$country = "Indonisia";
 
-$guide1 = "img/Local guides/wayanS.jpg";
-$guide1_name = "";
-$guide1_place = "";
+$pic1 = "img/img/bali/bali1.jpg";
+$pic1_name = "Bali";
+$pic1_place = "Indonisia";
 
-$guide2 = "img/Local guides/NyomanGM.jpg";
-$guide2_name = "";
-$guide2_place = "";
+$pic2 = "img/img/bali/bali2.jpg";
+$pic2_name = "Bali";
+$pic2_place = "Indonisia";
 
-$guide3 = "img/Local guides/wayanS.jpg";
-$guide3_name = "";
-$guide3_place = "";
+$pic3 = "img/img/bali/bali3.jpg";
+$pic3_name = "Ubud";
+$pic3_place = "Indonisia";
 $iframe_localisation = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31568.26907436767!2d115.24543978486727!3d-8.496110608896728!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd23d6a992a2b07%3A0xe028854c14cbced9!2sSaraswati%20Temple!5e0!3m2!1sfr!2stn!4v1681520141592!5m2!1sfr!2stn" width="600" height="450" style="border: 0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
 
 
