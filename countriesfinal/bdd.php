42e64b745f87fc17118a2bc2b9fa9ae89d978a7d
mabahej <mabahej.benhassine@insat.ucar.tn>
2023-04-28 16:51:28 +0100

    tryin

--- countriesfinal/bdd.php
+++ countriesfinal/bdd.php
@@ -1,15 +1,15 @@
 <?php class CBD
 {
     private static $_dbname = "clients";
-    private static $_user = "root";
-    private static $_pwd = "";
+    private static $_user = "mabahej";
+    private static $_pwd = "insat2023";
     private static $_host = "localhost";
     private static $_bdd = null;
 
     private function __construct()
     {
-        try {
-            self::$_bdd = new PDO("mysql:host=" . self::$_host . ";dbname=" . self::$_dbname . ";charset=utf8", self::$_user, self::$_pwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
+        try { //kenik madaltich ilport lil sql fasse5 port=3307"
+            self::$_bdd = new PDO("mysql:host=".self::$_host.";port=3307;dbname=".self::$_dbname.";charset=utf8", self::$_user, self::$_pwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
         } catch (PDOException $e) {
             die('Erreur : ' . $e->getMessage());
         }
@@ -19,7 +19,8 @@
     {
         if (!self::$_bdd) {
             new CBD();
-        }
+            
+}
         return (self::$_bdd);
     }
 }
--- countriesfinal/storeimg.php
+++ countriesfinal/storeimg.php
@@ -1,18 +1,18 @@
 <?php
 class storeimg {
 
-private $target_dir = "img/";
+/* private $target_dir = "img/";
 private $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 private $uploadOk = 1;
 private $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-function __construct()
+function __construct($vars)
 {
     $this->target_dir = "img/";
- $this->target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+ $this->target_file = $this->target_dir . basename($_FILES["fileToUpload"]["name"]);
  $this->uploadOk = 1;
- $this->imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+ $this->imageFileType = pathinfo($this->target_file,PATHINFO_EXTENSION);
 
-}
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -25,7 +25,7 @@ if(isset($_POST["submit"])) {
     }
 }
 // Check if file already exists
-if (file_exists($target_file)) {
+if (file_exists($this->target_file)) {
     echo "<center>Sorry, file already exists.</center>";
     $uploadOk = 0;
 }
@@ -35,22 +35,18 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "<center>Sorry, only JPG, JPEG, PNG & GIF files are allowed.</center>";
-    $uploadOk = 0;
-}
+
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "<center>Sorry, your file was not uploaded.</center>";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $this->target_file)) {
         echo "<center><i><h4>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</h4></i></center>";
     } else {
         echo "<center>Sorry, there was an error uploading your file.</font></center>";
     }}
-}
+}}
 ?>
 </body>
-</html>
+</html> */ }?>
