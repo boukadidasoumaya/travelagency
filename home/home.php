ad3724c4bc606bce8ca696d75f91125a8825eee1
arijzaidi <arij.zaidi@insat.ucar.tn>
2023-04-29 13:03:42 +0100

    modif

--- home/home.php
+++ home/home.php
@@ -287,46 +287,7 @@ include_once '../fragments/barre.php'
 <!-- Slides Section ends-->
 
 <!--------book now starts----->
-<section id="book">
-    <div class="container">
-        <div class="book-content-par">
-            <div class="book-counter">
-                <h3>Book Now</h3>
-                <div id="clockdiv">
-                    <div>
-                        <span class="days"></span>
-                        <div class="smalltext">Days</div>
 
-                    </div>
-                    <div>
-                        <span class="hours"></span>
-                        <div class="smalltext">Hours</div>
-
-                    </div>
-                    <div>
-                        <span class="minutes"></span>
-                        <div class="smalltext">Minutes</div>
-
-                    </div>
-                    <div>
-                        <span class="seconds"></span>
-                        <div class="smalltext">Seconds</div>
-
-                    </div>
-                </div>
-            </div>
-            <div class="book-form">
-                <form>
-                    <input type="text" id="fname" name="firstname" placeholder="Your Name...">
-                    <input type="text" id="lname" name="lastname" placeholder="Your Last Name...">
-                    <input type="text" id="pname" name="pays" placeholder="Your country...">
-                    <textarea name="subject" id="subject" placeholder="Write something..." style="height: 200px;"></textarea>
-                    <input type="submit" value="Submit">
-                </form>
-            </div>
-        </div>
-    </div>
-</section>
 <!--------book now section ends------->
 
 <?php include_once '../fragments/footer.php' ?>
