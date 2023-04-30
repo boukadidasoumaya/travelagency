e72c3d2ed74d301f5c56b5d3fc8fa1e8763bab0e
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2023-04-30 16:39:03 +0100

    updated booking.sql

--- booking/booking.sql
+++ booking/booking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 28 avr. 2023 à 18:50
+-- Généré le : dim. 30 avr. 2023 à 17:19
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -29,14 +29,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `booking` (
   `id_reservation` bigint(20) UNSIGNED NOT NULL,
-  `destination` varchar(255) NOT NULL,
   `date` date NOT NULL,
-  `num_passport` varchar(20) NOT NULL,
-  `client_first_name` varchar(255) NOT NULL,
-  `client_last_name` varchar(255) NOT NULL,
   `prix` decimal(10,2) NOT NULL,
-  `recu` blob DEFAULT NULL
+  `user_id` int(11) DEFAULT NULL,
+  `destination` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `booking`
+--
+
+INSERT INTO `booking` (`id_reservation`, `date`, `prix`, `user_id`, `destination`) VALUES
+(1, '2023-05-04', 3500.00, 1, 5),
+(2, '2023-05-03', 1700.00, 4, 6),
+(3, '2023-05-17', 1500.00, 4, 6),
+(4, '2023-05-04', 0.00, 1, 65),
+(5, '2023-05-25', 1650.00, 2, 6),
+(6, '2023-12-01', 3500.00, 1, 5),
+(7, '2023-05-06', 3500.00, 2, 5),
+(8, '2023-05-06', 1500.00, 2, 6),
+(9, '2023-05-04', 1500.00, 1, 6),
+(10, '2023-05-06', 1500.00, 4, 6);
 
 --
 -- Index pour les tables déchargées
@@ -46,7 +59,9 @@ CREATE TABLE `booking` (
 -- Index pour la table `booking`
 --
 ALTER TABLE `booking`
-  ADD PRIMARY KEY (`id_reservation`);
+  ADD PRIMARY KEY (`id_reservation`),
+  ADD KEY `booking_user_fk` (`user_id`),
+  ADD KEY `booking_destination_fk` (`destination`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -56,7 +71,18 @@ ALTER TABLE `booking`
 -- AUTO_INCREMENT pour la table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id_reservation` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_reservation` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_destination_fk` FOREIGN KEY (`destination`) REFERENCES `country` (`country_id`),
+  ADD CONSTRAINT `booking_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
