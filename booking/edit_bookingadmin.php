ca2fe17ebad05c27d780ab427349dfe4d0790b0f
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2023-05-06 19:35:15 +0100

    update

--- booking/edit_bookingadmin.php
+++ booking/edit_bookingadmin.php
@@ -1,7 +1,7 @@
 <?php
 include "bdd.php";
 require('booking_model.php');
-$db = new Booking();
+$db = new Booking($conn);
 $clients = $db->get_clients();
 
 ?>
@@ -23,45 +23,45 @@ $clients = $db->get_clients();
                 <tr>
                     <th scope="col">id_reservation</th>
                     <th scope="col">date</th>
-                    <th scope="col" >price-trip</th>
+                    <th scope="col">price-trip</th>
                     <th scope="col">user_id</th>
                     <th scope="col">destination</th>
                     <th scope="col">user_name</th>
                     <th scope="col">user_last_name</th>
                     <th scope="col">email</th>
-                   
-                    <th scope="col" >changes</th>
-                
-                    </tr>
+
+                    <th scope="col">changes</th>
+
+                </tr>
             </thead>
- 
-  <tbody>
-  <?php
-    foreach ($clients as $cl) :
-?>
-           <tr>
-           <th scope="row"><?php echo $cl['id_reservation']?></th>
-           <td><?php echo $cl['date']?></td>
-           <td><?php echo $cl['prix'] ?></td>
-           <td><?php echo $cl['user_id']?></td>
 
-           <td><?php echo $cl['destination']?></td>
-           <td><?php echo $cl['user_name']?></td>
-           <td><?php echo $cl['user_last_name']?></td>
-           <td><?php echo $cl['email']?></td>
+            <tbody>
+                <?php
+                foreach ($clients as $cl) :
+                ?>
+                    <tr>
+                        <th scope="row"><?php echo $cl['id_reservation'] ?></th>
+                        <td><?php echo $cl['date'] ?></td>
+                        <td><?php echo $cl['prix'] ?></td>
+                        <td><?php echo $cl['user_id'] ?></td>
 
-           <td> 
-           <button><a  class="btn btn-success" href="update_booking.php?updatedid=<?= $cl['id_reservation']?> ">update your trip</a></button>
-           <button><a href="delete_booking.php?deletedid=<?=$cl['id_reservation']?>">cancel your trip</a></button>
-           </td>
-           </tr>
-           
-   
-</tbody>
-  <?php endforeach; ?>
+                        <td><?php echo $cl['destination'] ?></td>
+                        <td><?php echo $cl['user_name'] ?></td>
+                        <td><?php echo $cl['user_last_name'] ?></td>
+                        <td><?php echo $cl['email'] ?></td>
 
-</table>
-            
+                        <td>
+                            <button><a class="btn btn-success" href="update_booking.php?updatedid=<?= $cl['id_reservation'] ?> ">update your trip</a></button>
+                            <button><a href="delete_booking.php?deletedid=<?= $cl['id_reservation'] ?>">cancel your trip</a></button>
+                        </td>
+                    </tr>
+
+
+            </tbody>
+        <?php endforeach; ?>
+
+        </table>
+
     </div>
 
 </body>
